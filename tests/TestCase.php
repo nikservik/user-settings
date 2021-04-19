be038c29e1a7890419ac4a00e3c0a720c16a7465
nikservik <nikservik@users.noreply.github.com>
2021-04-19 19:45:51 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Nikservik\UserSettings\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Nikservik\UserSettings\UserSettingsServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
